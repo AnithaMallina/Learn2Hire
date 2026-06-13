1b8997e38f59abed8004b83264381233d881869e
Anitha Mallina <anithamallina.s@gmail.com>
2026-06-13 11:20:10 +0530

    Fixed CORS issue

--- backend/server.js
+++ backend/server.js
@@ -3,20 +3,33 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const studentRoutes = require("./routes/studentRoutes");
 const trainerRoutes = require("./routes/trainerRoutes");
 const submissionRoutes = require("./routes/submissionRoutes");
+
 dotenv.config();
 
 connectDB();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // MUST come before routes
+// CORS FIX
+app.use(
+  cors({
+    origin: [
+      "https://learn2-hire-cmjz.vercel.app",
+      "https://learn2-hire-cmjz-7d4o9a752-anitha-mallina.vercel.app",
+      "http://localhost:5173",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -24,10 +37,20 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/student", studentRoutes);
 app.use("/api/trainer", trainerRoutes);
 app.use("/api/submission", submissionRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
-  res.json({
+  res.status(200).json({
     success: true,
     message: "Welcome to Learn2Hire API",
+  });
+});
+
+// Health Check
+app.get("/api", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "API Running Successfully",
   });
 });
 
