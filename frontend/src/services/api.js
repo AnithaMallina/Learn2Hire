d9ad9bee995cf51e9345e3bf7ae6441607c4b9f1
Anitha Mallina <anithamallina.s@gmail.com>
2026-06-13 10:06:29 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://learn2-hireee-pn2el625c-anitha-mallina.vercel.app/api",
 });
 
 export default API;
